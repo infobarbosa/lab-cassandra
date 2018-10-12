5c29948a42d4ee3c4d936cc2cdb6062a7777f70a
Marcelo Barbosa Pinto <infobarbosa@yahoo.com.br>
2018-10-12 15:51:17 -0300

    removendo target

--- target/classes/cassandra.sql
+++
@@ -1,6 +0,0 @@
-CREATE KEYSPACE lab_cassandra WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}  AND durable_writes = true;
-
-CREATE TABLE lab_cassandra.chave_valor (
-    chave uuid PRIMARY KEY,
-    valor text
-);
